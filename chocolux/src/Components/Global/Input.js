14db615f1f26b2c7bdc8e25c97884430b94f0a21
Salvador Camiege <camiegesalvador@gmail.com>
2025-04-17 12:23:02 +0100

    novas telas criadas

--- chocolux/src/Components/Global/Input.js
+++ chocolux/src/Components/Global/Input.js
@@ -1,14 +1,39 @@
+import { useState } from 'react';
 import styles from './Input.module.css';
 
 function Input() {
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = () => {
+    // Regex simples para verificar formato de e-mail
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!email) {
+      setError('Por favor, preencha o e-mail.');
+    } else if (!emailRegex.test(email)) {
+      setError('E-mail inválido.');
+    } else {
+      setError('');
+      alert(`E-mail enviado com sucesso: ${email}`);
+      setEmail(''); // limpa o campo após envio
+    }
+  };
+
   return (
     <div className={styles.inputWrapper}>
       <input
         type="email"
         className={styles.inputEmail}
         placeholder="Digite seu email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
-      <button className={styles.sendButton}>
+      <button
+        className={styles.sendButton}
+        type="button"
+        onClick={handleSubmit}
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="18"
@@ -25,6 +50,9 @@ function Input() {
           <polyline points="12 5 19 12 12 19" />
         </svg>
       </button>
+
+      {/* Exibe erro, se existir */}
+      {error && <p className={styles.errorMessage}>{error}</p>}
     </div>
   );
 }
--- chocolux/src/Components/Leyouts/Rodape.js
+++ chocolux/src/Components/Leyouts/Rodape.js
@@ -1,45 +1,74 @@
 import styles from './Rodape.module.css';
 import Input from '../Global/Input';
-
 import { FaFacebookF, FaLinkedinIn, FaGithub } from 'react-icons/fa';
 
 function Rodape() {
     return (
         <div className={styles.Rodape}>
             <div className={styles.leftContent}>
-                <Input />
+                {/* Input e redes sociais lado a lado */}
+                <div className={styles.inputSocialContainer}>
+                    <Input />
 
-                {/* Redes sociais ao lado do input */}
-                <ul className={styles.socialList}>
-                    <li>
-                        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                            <FaFacebookF className={styles.icon} />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-                            <FaLinkedinIn className={styles.icon} />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://github.com" target="_blank" rel="noopener noreferrer">
-                            <FaGithub className={styles.icon} />
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            
-            {/* Menu de Navegação (NavBar) abaixo do input */}
-            <div className={styles.navBar}>
-                
-                <ul>
-                    <h1>Menu</h1>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/Sobre">Sobre</a></li>
-                    <li><a href="/Contatos">Chocolates</a></li>
-                    <li><a href="/Contatos">Depoimentos</a></li>
-                    <li><a href="/Contatos">Contatos-nos</a></li>
-                </ul>
+                    <ul className={styles.socialList}>
+                        <li>
+                            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                                <FaFacebookF className={styles.icon} />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+                                <FaLinkedinIn className={styles.icon} />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+                                <FaGithub className={styles.icon} />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                {/* Menu e colunas lado a lado */}
+                <div className={styles.menuAndColumns}>
+                    <div className={styles.navBar}>
+                        <h1 className={styles.h1}>Menu</h1>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/Sobre">Sobre</a></li>
+                            <li><a href="/Contatos">Chocolates</a></li>
+                            <li><a href="/Contatos">Depoimentos</a></li>
+                            <li><a href="/Contatos">Contatos-nos</a></li>
+                        </ul>
+                    </div>
+
+                    <div className={styles.columnsContainer}>
+                        <div className={styles.column}>
+                            <h1 className={styles.h1}>Produtos</h1>
+                            <ul>
+                                <li><a href="#">Chocolate ao Leite</a></li>
+                                <li><a href="#">Chocolate Amargo</a></li>
+                                <li><a href="#">Bombons</a></li>
+                            </ul>
+                        </div>
+                        <div className={styles.column}>
+                            <h1 className={styles.h1}>Empresa</h1>
+                            <ul>
+                                <li><a href="#">Quem somos</a></li>
+                                <li><a href="#">Missão</a></li>
+                                <li><a href="#">Visão</a></li>
+                            </ul>
+                        </div>
+                        <div className={styles.column}>
+                            <h1 className={styles.h1}>Atendimento</h1>
+                            <ul>
+                                <li><a href="#">Fale Conosco</a></li>
+                                <li><a href="#">Suporte</a></li>
+                                <li><a href="#">FAQ</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
